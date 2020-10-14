ff826dba46372489c6a6152e8b7c5737e4b7c806
Murphy <sydneyporter74@yahoo.com>
2020-10-14 19:44:33 -0400

    package.json

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
-const PORT = process.env.PORT || 3001;
+const htmlRoutes = require('./routes/htmlRoute');
+const apiRoutes = require("./routes/apiRoute");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 
 app.listen(PORT, () => {
